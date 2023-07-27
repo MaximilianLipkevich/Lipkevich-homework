3fbde9c309cf610852ef4c7a57f0e4d51acfafef
delacroixxxx <kravez1707@gmail.com>
2023-07-27 21:55:13 +0300

    Sending thursday homework

---
+++ Homework july 27.py
@@ -0,0 +1,23 @@
+#task1
+name = "Maximilian"
+dayOfWeek = "Thursday"
+
+method1 = f"Good day {name}! {dayOfWeek} is a perfect day to learn some python.\n"
+method2 = "Good day {1}! {0} is a perfect day to learn some python.\n".format(dayOfWeek, name)
+method3 = "Good day %s! %s is a perfect day to learn some python.\n" % (name, dayOfWeek)
+print(method1)
+
+#task2
+surname = "Lipkevich"
+print("Greeetings, my name is " + name + " " + surname + "\n")
+
+#task3
+a = 5
+b = 2
+print(5 + 2) #addition
+print(5 - 2) #subtraction
+print(5 * 2) #multiplication
+print(5 / 2) #division
+print(5 ** 2) #exponenta
+print(5 % 2) #modulus
+print(5 // 2) #floor division
