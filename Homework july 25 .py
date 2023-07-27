418b7c71cb729de9b283cc860f0dd5356ed3400c
delacroixxxx <kravez1707@gmail.com>
2023-07-27 21:28:34 +0300

    Added task 2

--- Homework july 25 .py
+++ Homework july 25 .py
@@ -1,2 +1,11 @@
-print("#########\n#\t\t#\n#\t\t#\n#\t\t#\n#########\n")
+#task2
+# what's up
+print("what's up?")
+# 110
+print(85 + 25)
+# my_name_is_max!
+print("my", "name", "is", "max", sep="_", end="!")
+
+#task3
+print("\n\n#########\n#\t\t#\n#\t\t#\n#\t\t#\n#########\n")
 print("#\t\t#\n#\t\t#\n#########\n#\t\t#\n#\t\t#\n")
