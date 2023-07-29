0d5351c386ff4a2105b42420051e7f55e3345aa8
delacroixxxx <kravez1707@gmail.com>
2023-07-29 20:43:43 +0300

    Sending saturday homework lesson 5

---
+++ homework july 29 lesson 5.py
@@ -0,0 +1,29 @@
+import random
+
+# task1
+generatedNumber = random.randint(1, 10)
+userNumber = int(input("Enter your number:\n"))
+
+if generatedNumber == userNumber:
+    print("Congrats! You guessed the number correctly!")
+else:
+    print(f"The correct number was {generatedNumber}. You lost.")
+
+# task2
+name = input("What is your name?\n")
+age = int(input("What is your age?\n"))
+
+print(f"Hello {name.capitalize()}, on your next birthday you’ll be {age + 1} years old.")
+
+# task3
+string = input("Enter your sentence:\n")
+def shuffleLetters(string):
+    stringLetters = [i for i in string]
+    stringIndexes = [i for i in range(0, len(stringLetters))]
+    random.shuffle(stringIndexes)
+    result = ''
+    for n in stringIndexes:
+        result += string[n]
+    return result
+
+print(shuffleLetters(string), shuffleLetters(string), shuffleLetters(string), shuffleLetters(string), shuffleLetters(string))
