59da63b77ce85002d20eb747f56c5d00de736ba2
delacroixxxx <kravez1707@gmail.com>
2023-08-01 22:53:46 +0300

    Completed task 2

---
+++ Homework August 1 Lesson 6/Task 2.py
@@ -0,0 +1,24 @@
+import random
+
+list1 = []
+list2 = []
+i = 0
+
+while i < 10:
+    list1.append(random.randint(0, 10))
+    list2.append(random.randint(0, 10))
+    i += 1
+
+list3 = []
+commonIndex = 0
+
+while len(list1) > commonIndex:
+    if list1[commonIndex] in list2:
+        list3.append(list1[commonIndex])
+        commonIndex += 1
+    else:
+        commonIndex += 1
+
+print(f'First list is {list1}')
+print(f'Second list is {list2}')
+print(f'Common numbers are {list(set(list3))}')
