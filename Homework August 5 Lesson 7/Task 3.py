96685bb8964e58a5e1c63214df6a730902ef19b4
delacroixxxx <kravez1707@gmail.com>
2023-08-05 20:18:52 +0300

    Completed task 3 and 4

---
+++ Homework August 5 Lesson 7/Task 3.py
@@ -0,0 +1,3 @@
+result_list = [(i, j := i * i) for i in range(1, 11)]
+
+print(result_list)
---
+++ Homework August 5 Lesson 7/Task 4.py
@@ -0,0 +1,6 @@
+weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+weekdaysDict = {number + 1: day for (number, day) in enumerate(weekdays)}
+reverseWeekdaysDict = {day: number + 1 for (number, day) in enumerate(weekdays)}
+
+print(reverseWeekdaysDict)
