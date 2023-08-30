42d27434c9653467dac162016c8704f7c3ce1250
delacroixxxx <kravez1707@gmail.com>
2023-08-30 22:52:53 +0300

    Finished task 2 and 3

---
+++ Homework August 22 Lesson 13/Task 3.py
@@ -0,0 +1,17 @@
+def choose_func(nums: list, func1, func2):
+    if all(num > 0 for num in nums):
+        return func1(nums)
+    else:
+        return func2(nums)
+
+def square_nums(nums):
+    return [num ** 2 for num in nums]
+
+def remove_negatives(nums):
+    return [num for num in nums if num > 0]
+
+nums1 = [1, 2, 3, 4, 5]
+nums2 = [1, -2, 3, -4, 5]
+
+assert choose_func(nums1, square_nums, remove_negatives) == [1, 4, 9, 16, 25]
+assert choose_func(nums2, square_nums, remove_negatives) == [1, 3, 5]
